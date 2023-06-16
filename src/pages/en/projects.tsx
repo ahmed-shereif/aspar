24375937716c64a9538e4900aa1cf298c0153b56
aSherif310 <asherif@tech4health.io>
2023-06-16 22:40:05 +0300

    adding img

--- src/pages/en/projects.tsx
+++ src/pages/en/projects.tsx
@@ -29,8 +29,8 @@ const SLIDER_DATA_B = [
 
 
 const SLIDER_DATA_A = [
-  { id: 441, src: '/c-1-h.jpg' },
-  { id: 442, src: '/c-1-h.jpg' },
+  { id: 442, src: '/3in-1-1.jpg' },
+  { id: 441, src: '/3in-1-2.png' },
 
 ]
 
@@ -88,7 +88,7 @@ export default function Projects() {
 
 
           <div className='w-full mt-5'>
-            <motion.div viewport={{ once: true }} whileInView={{ x: isMobile ? [50, 0] : [100, 0], opacity: [0, 1] }} transition={{ delay: 0.1, ...TRANS }} initial={{ opacity: 0 }} className='h-80 lg:h-[62vh]  min-w-full  overflow-hidden'>
+            <motion.div viewport={{ once: true }} whileInView={{ x: isMobile ? [50, 0] : [100, 0], opacity: [0, 1] }} transition={{ delay: 0.1, ...TRANS }} initial={{ opacity: 0 }} className='h-80 lg:h-[64vh]  min-w-full  overflow-hidden'>
               <Swiper pagination={{ clickable: true }} modules={[Autoplay, Pagination]} autoplay={{ delay: 2500, disableOnInteraction: false, }} className="mySwiper w-full h-full select-none">
                 {
                   SLIDER_DATA_A.map(slid =>
